99380a2be22d2206604a23a42a17b06fd0bdde97
Hugues Demers <hdemers@gmail.com>
2012-07-22 17:43:02 -0400

    Minor tweak to connection handling.

--- cloudly/aws/sns.py
+++ cloudly/aws/sns.py
@@ -1,13 +1,13 @@
 import boto
 import json
 
+connection = boto.connect_sns()
+
 
 def publish(topic_arn, message, subject=None):
     if message:
-        connection = boto.connect_sns()
         connection.publish(topic_arn, json.dumps(message), subject)
 
 
 def subscribe(topic_arn, protocol, endpoint):
-    connection = boto.connect_sns()
     connection.subscribe(topic_arn, protocol, endpoint)
