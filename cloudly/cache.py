cd45585233acfe7db1f757b244e2edba8dbb6f6b
Hugues Demers <hdemers@gmail.com>
2012-07-27 14:26:03 -0400

    Use environment variable REDIS_SERVER.

--- cloudly/cache.py
+++ cloudly/cache.py
@@ -7,7 +7,10 @@ from cloudly.memoized import Memoized
 
 @Memoized
 def get_conn():
-    ip_addresses = ec2.find_service_ip('redis-server') or ["127.0.0.1"]
+    ip_addresses = (os.environ.get("REDIS_SERVER") or
+                    ec2.find_service_ip('redis-server') or 
+                    ["127.0.0.1"])
+
     redis_url = os.getenv('REDISTOGO_URL',  # Set when on Heroku.
                           'redis://{}:6379'.format(ip_addresses[0]))
     return pyredis.from_url(redis_url)
