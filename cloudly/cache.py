d96714270863a2a2dbf767b87a9bf25a6d223cfd
Hugues Demers <hdemers@gmail.com>
2012-08-05 18:22:17 -0400

    Add MemCache proxy, add redis.jget and redis.jset.
    The jset and jget functions are wrappers that serialize to/from
    JSON before calling redis.set and redis.get.

--- cloudly/cache.py
+++ cloudly/cache.py
@@ -1,5 +1,11 @@
-import os
+"""This module provide access to redis and memcache servers with some sugar
+coating.
+"""
 
+import os
+import json
+
+import memcache
 import redis as pyredis
 
 from cloudly.aws import ec2
@@ -10,7 +16,7 @@ log = logger.init(__name__)
 
 
 @Memoized
-def get_conn():
+def get_redis_connection():
     """ Get a connection to a Redis server. The priority is:
         - look for an environment variable REDIS_HOST, else
         - look for an EC2 hosted server offering the service 'redis', else
@@ -23,7 +29,7 @@ def get_conn():
         service_ips = []
 
     ip_address = (os.environ.get("REDIS_HOST") or
-                  service_ips[0] if service_ips else None or 
+                  service_ips[0] if service_ips else None or
                   "127.0.0.1")
 
     redis_url = os.getenv('REDISTOGO_URL',  # Set when on Heroku.
@@ -33,4 +39,45 @@ def get_conn():
     return pyredis.from_url(redis_url)
 
 
-redis = get_conn()
+redis = get_redis_connection()
+
+
+# Add some utility function to module redis. These function first serialize
+# to/from JSON the given object.
+def redis_jget(key):
+    value = redis.get(key)
+    return json.loads(value) if value else None
+
+redis.jset = lambda key, obj: redis.set(key, json.dumps(obj))
+redis.jget = redis_jget
+
+
+@Memoized
+def get_memcache_connection():
+    return memcache.Client(['127.0.0.1:11211'], debug=0)
+
+
+class MemProxy(object):
+    def __init__(self):
+        self.cache = get_memcache_connection()
+
+    def set(self, key, obj, time=0):
+        if key.find(" ") > -1:
+            raise ValueError("A memcached key cannot contain spaces.")
+        return self.cache.set(key.encode("utf-8"), obj, time=time,
+                              min_compress_len=1024)
+
+    def get(self, key):
+        return self.cache.get(key.encode("utf-8"))
+
+    def set_multi(self, mapping, time=0):
+        return self.cache.set_multi(mapping, time, min_compress_len=1024)
+
+    def get_multi(self, keys):
+        return self.cache.get_multi(keys)
+
+    def delete(self, key):
+        self.cache.delete(key)
+
+
+memcache = MemProxy()
