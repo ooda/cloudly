50d50bc428d57b16269f72e4be022cfa1df2b16f
Hugues Demers <hdemers@gmail.com>
2012-08-27 09:48:22 -0400

    Fix dependencies.
    [#35019745]

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     scripts=['bin/sshaws', 'bin/launch'],
     install_requires=['distribute', 'boto', 'redis', 'rq', 'couchdb',
-                      'python-memcached']
+                      'python-memcached', 'isodate']
 )
