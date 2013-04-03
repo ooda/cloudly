6df61a49a1e2e23a01e0dcf4b14dd8bce4c1f619
Hugues Demers <hdemers@gmail.com>
2013-04-03 11:18:48 -0400

    Pin dependencies in setup.py and requirements.txt
    Add missing pusher requirement.
    Some package versions were update in the process.

--- setup.py
+++ setup.py
@@ -15,6 +15,14 @@ setup(
     license="Proprietary",
     packages=find_packages(exclude=('tests', 'docs')),
     scripts=['bin/sshaws', 'bin/launch'],
-    install_requires=['distribute', 'boto', 'redis', 'rq', 'couchdb',
-                      'python-memcached', 'isodate', 'cuisine']
+    install_requires=[
+        'boto==2.8.0',
+        'redis==2.7.2',
+        'rq==0.3.7',
+        'couchdb==0.8',
+        'python-memcached==1.48',
+        'isodate==0.4.9',
+        'cuisine==0.5.6',
+        'pusher==0.7'
+    ]
 )
